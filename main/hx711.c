22156d2a81a4408272cfcbd3b7e8b3e807b80c97
emblink <emblink182@gmail.com>
2019-06-29 01:58:17 +0300

    add hx711 api and weight task

--- main/hx711.c
+++ main/hx711.c
@@ -1,1 +1,33 @@
 #include "hx711.h"
+#include "stdint.h"
+
+void hx711Init(void)
+{
+
+}
+
+bool hx711GetStatus(void)
+{
+    return true;
+}
+
+bool hx711ReadChannel(Hx711Channel channel, uint32_t *data)
+{
+    return true;
+}
+
+/* When PD_SCK pin changes from low to high
+and stays at high for longer than 60µs, HX711
+enters power down mode (Fig.3). */
+void hx711PowerDown(void)
+{
+
+}
+
+/* After a reset or power-down event, input
+selection is default to Channel A with a gain of 128. */
+void hx711Resume(void)
+{
+
+}
+
--- main/main.c
+++ main/main.c
@@ -12,11 +12,37 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "driver/gpio.h"
+#include "driver/spi_master.h"
 #include "hx711.h"
 
 #define LED_PIN (GPIO_NUM_2)
+#define VSPI_MOSI (GPIO_NUM_23)
+#define VSPI_MISO (GPIO_NUM_19)
+#define VSPI_SCLK (GPIO_NUM_18)
 
-static void ledBlinkTask(void* arg)
+static spi_device_handle_t spi;
+
+static const spi_bus_config_t spiConfig = {
+    .mosi_io_num = VSPI_MOSI,
+    .miso_io_num = VSPI_MISO,
+    .sclk_io_num = VSPI_SCLK,
+    .quadwp_io_num = -1,
+    .quadhd_io_num = -1,
+    .max_transfer_sz = 0,
+    .flags = 0,
+    .intr_flags = 0
+};
+
+static spi_device_interface_config_t devcfg = {
+    .clock_speed_hz = 1*1000*1000, // Tmin = 0,2us = 5Mhz, set to 1Mhz
+    .mode = 0,                     // SPI mode 0
+    .spics_io_num = -1,            // no CS pin
+    .queue_size = 2,
+    .flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA,
+};
+
+
+static void ledBlinkTask(void *arg)
 {
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
     gpio_pulldown_dis(LED_PIN);
@@ -32,12 +58,32 @@ static void ledBlinkTask(void* arg)
     }
 }
 
+static void weightTask(void *arg)
+{
+    hx711Init();
+    static uint32_t value = 0;
+    for (;;) {
+        hx711ReadChannel(Hx711ChannelA128, &value);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main()
 {
     printf("Started!\n");
 
+    printf("Spi Init\n");
+    esp_err_t err = ESP_OK;
+    err |= spi_bus_initialize(VSPI_HOST, &spiConfig, 0);
+    printf("spi_bus_initialize Passed!\n");
+    err |= spi_bus_add_device(VSPI_HOST, &devcfg, &spi);
+    printf("spi_bus_add_device Passed!\n");
+    ESP_ERROR_CHECK(err);
+    printf("Spi Init Success!\n");
+
     //start led blink task
-    xTaskCreate(ledBlinkTask, "ledBlinkTask", 2048, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(ledBlinkTask, "ledBlinkTask", 1024, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(weightTask, "weightTask", 2048, NULL, tskIDLE_PRIORITY + 1, NULL);
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
