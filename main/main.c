163e22ed0e40117432f58ced9ff5b20bc9a7c9b1
emblink <emblink182@gmail.com>
2019-07-03 01:11:40 +0300

    WIP: add oled lib, port code from stm32

--- main/main.c
+++ main/main.c
@@ -12,14 +12,43 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "driver/gpio.h"
-#include "driver/spi_master.h"
+#include "driver/i2c.h"
 #include <esp32/rom/ets_sys.h>
 #include "hx711.h"
+#include "oled.h"
 
 #define LED_PIN (GPIO_NUM_2)
 #define HX711_DATA_SIZE 120
 
 static uint32_t hx711Data[HX711_DATA_SIZE] = {0};
+
+static esp_err_t i2c_master_init()
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    return i2c_driver_install(i2c_master_port, conf.mode,
+                              I2C_MASTER_RX_BUF_DISABLE,
+                              I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+static esp_err_t i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDR << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
 
 static void ledBlinkTask(void *arg)
 {
@@ -59,6 +88,10 @@ static void weightTask(void *arg)
     };
 
     hx711Init(&handle);
+
+    //OLED_Init();
+    OLED_FillScreen(White);
+    OLED_UpdateScreen();
 
     for (;;) {
         static uint32_t index = 0;
